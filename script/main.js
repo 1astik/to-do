ef2ddcec4e2612bc34857689a00346efa5b20dce
1astik <marik_s1996@mail.ru>
2019-12-04 17:00:55 +0300

    First final project

--- script/main.js
+++ script/main.js
@@ -2,7 +2,6 @@
 
 
 window.addEventListener( "DOMContentLoaded", () => {
-    console.log(new Date());
     // const tasks = [
     //     {
     //         id: 0,
@@ -35,9 +34,22 @@ window.addEventListener( "DOMContentLoaded", () => {
     // ];
     // const local_tasks = JSON.stringify(tasks);
     // localStorage.setItem('tasks',local_tasks);
-    const tasks = JSON.parse( localStorage.getItem( 'tasks' ) );
 
     const my_list = document.querySelector( ".my-list" );
+    const form = document.querySelector( '.form' );
+    let tasks = [];
+
+    async function getData() {
+        const tasks = await JSON.parse( localStorage.getItem( 'tasks' ) );
+
+        return tasks
+    }
+
+    getData()
+        .then( data => {
+        viewTasks( data );
+        tasks = data;
+    } );
 
 
     function createCard( { id, category, title, body, date } ) {
@@ -80,45 +92,41 @@ window.addEventListener( "DOMContentLoaded", () => {
 
 
     function viewTasks( tasks ) {
+
         tasks.forEach( elem => {
             my_list.append( createCard( elem ) );
         } )
     }
 
-    viewTasks( tasks );
 
-
-    const form = document.querySelector( '.form' );
-
-    form.addEventListener( "submit", onFormSubmit );
-
-    function onFormSubmit( e ) {
+    form.addEventListener( "submit", e => {
         e.preventDefault();
         const obj = [{}];
-        const text_category = document.getElementById( 'form101' );
-        const text_title = document.getElementById( 'form102' );
-        const text_body = document.getElementById( 'form103' );
+        const text_category = document.getElementById( 'form101' ).value.trim();
+        const text_title = document.getElementById( 'form102' ).value.trim();
+        const text_body = document.getElementById( 'form103' ).value.trim();
+        
+        if ( text_title === '' || text_category === '' || text_body === '' ) {
+            alert('Введите данные')
+        }else {
+            tasks.length === 0 ? obj[0].id = 0 : obj[0].id = ( tasks[tasks.length - 1].id + 1 );
+            obj[0].category = text_category;
+            obj[0].title = text_title;
+            obj[0].body = text_body;
+            obj[0].date = `${ new Date().getDay() }.${ new Date().getMonth() }.${ new Date().getFullYear() }  ${ new Date().getHours() }:${ new Date().getMinutes() }`;
 
-        tasks.length === 0 ? obj[0].id = 0: obj[0].id = (tasks[tasks.length-1].id + 1);
-        obj[0].category = text_category.value;
-        obj[0].title = text_title.value;
-        obj[0].body = text_body.value;
-        obj[0].date = `${ new Date().getDay() }.${ new Date().getMonth() }.${ new Date().getFullYear() }  ${ new Date().getHours() }:${ new Date().getMinutes() }`;
+            form.reset();
 
+            tasks.push( obj[0] );
+            localStorage.setItem( 'tasks', JSON.stringify( tasks ) );
 
-        form.reset();
-
-        tasks.push( obj[0] );
-        localStorage.setItem( 'tasks', JSON.stringify( tasks ) );
-
-        viewTasks( obj );
-
-
-    }
+            viewTasks( obj );
+        }
+    } );
 
 
     function deleteTasks( id ) {
-        const index = tasks.findIndex( item => item.id === +id);
+        const index = tasks.findIndex( item => item.id === +id );
         tasks.splice( index, 1 );
     }
 
@@ -128,7 +136,7 @@ window.addEventListener( "DOMContentLoaded", () => {
 
         if ( target.classList.contains( 'confirm' ) ) {
             let del = target.parentElement;
-            deleteTasks(target.id);
+            deleteTasks( target.id );
             localStorage.setItem( 'tasks', JSON.stringify( tasks ) );
             while ( !del.classList.contains( 'card' ) ) {
                 del = del.parentElement;
